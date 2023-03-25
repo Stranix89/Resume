02c2983a18468b7413d1092f8ae0c7839ac95518
Stranix89 <zmohaxz82@gmail.com>
2023-03-25 22:09:30 +0300

    portfolio

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,31 +1,77 @@
 <template>
-  <div class="portfolio">
-    <v-col>
-      <div class="portfolio-block">
-        В данный момент заканчиваю обучение по направлению frontend разработчик.
-        Ранее год изучал самостоятельно HTML, CSS (SCSS), JS и делал мини проекты.
-        Так же сейчас изучаю и делаю первые проекты на NUXT.
-        Хочу попасть в команду с которой буду на одной волне, чтобы раскрыть это направление по максимуму.
-
-        Хобби:
-        Книги, саморазвитие,съемка с квадрокоптера.
-
-        Личные и профессиональные качества:
-        Дисциплинированный
-        Уважаю свое и чужое время, поэтому работу стараюсь выполнить качественно и в срок.
-        Люблю учиться и узнавать новое.
-        Умею высказывать аргументированно свое мнение.
-        Спокойно воспринимаю критику.
-        Умению организовывать работу группы людей
-        Высокая работоспособность
-        Умение быстро реагировать в сложных ситуациях
-        Высокая обучаемость и восприимчивость
-        Широкий технологический кругозор
-        Работа с большим количеством информации
-        Умение работать в команде.
-      </div>
-    </v-col>
-  </div>
+  <v-container
+    class="portfolio"
+    fluid
+    style="min-height: 434px"
+  >
+    <v-fade-transition mode="out-in">
+      <v-row>
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="..//RockPaperScissors.jpg"
+              class="portfolio-img"
+              ></v-img>
+              <v-card-title class="portfolio-img-text">
+                <a href="https://github.com/Stranix89/Rock-Paper-Scissors">https://github.com/Stranix89/Rock-Paper-Scissors</a>
+            </v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="..//cinema.jpg"
+              height="467px"
+              width="900px"
+              cover
+              class="portfolio-img"
+            ></v-img>
+            <v-card-title class="portfolio-img-text">
+              <a href="https://github.com/Stranix89/react-cinema">https://github.com/Stranix89/react-cinema</a>
+            </v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="15">
+          <v-card>
+            <v-img
+              src="..//notebook.jpg"
+              cover
+              class="portfolio-img"
+            ></v-img>
+            <v-card-title class="portfolio-img-text">
+              <a href="https://github.com/Stranix89/Notebook">https://github.com/Stranix89/Notebook</a>
+            </v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="..//layout1.jpg"
+              cover
+              height="467px"
+              class="portfolio-img"
+            ></v-img>
+            <v-card-title class="portfolio-img-text">
+              <a href="https://github.com/Stranix89/Layout-1">https://github.com/Stranix89/Layout-1</a>
+            </v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="..//layout2.jpg"
+              cover
+              height="467px"
+              class="portfolio-img"
+            ></v-img>
+            <v-card-title class="portfolio-img-text">
+              <a href="https://github.com/Stranix89/Layout-1">https://github.com/Stranix89/Layout-1</a>
+            </v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-fade-transition>
+  </v-container>
 </template>
 
 <style scoped>
@@ -34,8 +80,11 @@ background-color: #C7B9FF;
 padding: 40px;
 margin: 50px  0;
 }
-.portfolio-block {
-border: 1px solid black;
+.portfolio-img {
 padding: 15px;
+/*width: 900px;*/
+}
+.portfolio-img-text{
+  text-align: center;
 }
 </style>
